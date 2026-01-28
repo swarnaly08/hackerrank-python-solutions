f3462abfbea11d1bb85f691a3730548ff11f081a
Swarnaly Roy <swarnalyroy667@gmail.com>
2026-01-28 20:51:54 +0530

    added string split and join question solution

---
+++ StringSplitandJoin.py
@@ -0,0 +1,12 @@
+def split_and_join(line):
+    
+    words = line.split(" ")
+    
+    result = "-".join(words)
+    
+    return result
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
